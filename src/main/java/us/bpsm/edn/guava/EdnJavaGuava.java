af3338e6baefa1064e0f3f65f876ec786c233822
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2013-02-24 14:57:42 +0100

    replace conditionals with polymorphism
    What exactly happens when newParserConfigBuilder(NilPolicy) is run
    depends only on NilPolicy, therefore make it a method of NilPolicy.
    
    Also, remove some (probably premature) "optimization" that cached
    instance of the Guava*Factory classes in static finals so as to be
    able to share instances. On further thought, I don't expect the
    creation of parser configurations to occur so often that it would
    benefit.
    
    This simplifies the implementation.

--- src/main/java/us/bpsm/edn/guava/EdnJavaGuava.java
+++ src/main/java/us/bpsm/edn/guava/EdnJavaGuava.java
@@ -7,55 +7,21 @@ import us.bpsm.edn.parser.Parser.Config;
 
 public class EdnJavaGuava {
 
-    private static final Factory GUAVA_FAILS_ON_NIL_LIST_FACTORY = new GuavaFailsOnNilListFactory();
-    private static final Factory GUAVA_FAILS_ON_NIL_SET_FACTORY = new GuavaFailsOnNilSetFactory();
-    private static final Factory GUAVA_FAILS_ON_NIL_MAP_FACTORY = new GuavaFailsOnNilMapFactory();
-    private static final Factory GUAVA_FAILS_ON_NIL_VECTOR_FACTORY = new GuavaFailsOnNilVectorFactory();
-
-    private static final Factory GUAVA_SKIPS_NIL_LIST_FACTORY = new GuavaSkipsNilListFactory();
-    private static final Factory GUAVA_SKIPS_NIL_SET_FACTORY = new GuavaSkipsNilSetFactory();
-    private static final Factory GUAVA_SKIPS_NIL_MAP_FACTORY = new GuavaSkipsNilMapFactory();
-    private static final Factory GUAVA_SKIPS_NIL_VECTOR_FACTORY = new GuavaSkipsNilVectorFactory();
-
-    private static final Config GUAVA_FAILS_ON_NIL_CONFIGURATION = newParserConfigBuilder(
-            NilPolicy.FAILS_ON_NIL).build();
-
-    private static final Config GUAVA_SKIPS_NIL_CONFIGURATION = newParserConfigBuilder(
-            NilPolicy.SKIPS_NIL).build();
+    public static final NilPolicy DEFAULT_NIL_POLICY = NilPolicy.FAILS_ON_NIL;
 
     /**
      * Return a Parser.Config.Builder pre-configured to build Guava collections.
      */
     public static Builder newParserConfigBuilder(NilPolicy policy) {
-        switch (policy) {
-        case FAILS_ON_NIL:
-            return Parsers.newParserConfigBuilder()
-                    .setListFactory(GUAVA_FAILS_ON_NIL_LIST_FACTORY)
-                    .setSetFactory(GUAVA_FAILS_ON_NIL_SET_FACTORY)
-                    .setMapFactory(GUAVA_FAILS_ON_NIL_MAP_FACTORY)
-                    .setVectorFactory(GUAVA_FAILS_ON_NIL_VECTOR_FACTORY);
-        case SKIPS_NIL:
-            return Parsers.newParserConfigBuilder()
-                    .setListFactory(GUAVA_SKIPS_NIL_LIST_FACTORY)
-                    .setSetFactory(GUAVA_SKIPS_NIL_SET_FACTORY)
-                    .setMapFactory(GUAVA_SKIPS_NIL_MAP_FACTORY)
-                    .setVectorFactory(GUAVA_SKIPS_NIL_VECTOR_FACTORY);
-        }
-        throw new IllegalArgumentException("policy = " + policy);
+		return policy.newParserConfigBuilder();
     }
 
     public static Config defaultConfiguration() {
-        return GUAVA_FAILS_ON_NIL_CONFIGURATION;
+		return DEFAULT_NIL_POLICY.newParserConfigBuilder().build();
     }
 
     public static Config defaultConfiguration(NilPolicy policy) {
-        switch (policy) {
-        case FAILS_ON_NIL:
-            return GUAVA_FAILS_ON_NIL_CONFIGURATION;
-        case SKIPS_NIL:
-            return GUAVA_SKIPS_NIL_CONFIGURATION;
-        }
-        throw new IllegalArgumentException("policy = " + policy);
+		return policy.newParserConfigBuilder().build();
     }
 
 }
--- src/main/java/us/bpsm/edn/guava/NilPolicy.java
+++ src/main/java/us/bpsm/edn/guava/NilPolicy.java
@@ -1,6 +1,33 @@
 package us.bpsm.edn.guava;
 
+import us.bpsm.edn.parser.Parser.Config.Builder;
+import us.bpsm.edn.parser.Parser.Config;
+import us.bpsm.edn.parser.Parsers;
+
 public enum NilPolicy {
-    FAILS_ON_NIL,
-    SKIPS_NIL;
+
+    FAILS_ON_NIL() {
+        @Override
+        Builder newParserConfigBuilder() {
+            return Parsers.newParserConfigBuilder()
+                .setListFactory(new GuavaFailsOnNilListFactory())
+                .setSetFactory(new GuavaFailsOnNilSetFactory())
+                .setMapFactory(new GuavaFailsOnNilMapFactory())
+                .setVectorFactory(new GuavaFailsOnNilVectorFactory());
+        }
+    },
+
+    SKIPS_NIL() {
+        @Override
+        Builder newParserConfigBuilder() {
+            return Parsers.newParserConfigBuilder()
+                .setListFactory(new GuavaSkipsNilListFactory())
+                .setSetFactory(new GuavaSkipsNilSetFactory())
+                .setMapFactory(new GuavaSkipsNilMapFactory())
+                .setVectorFactory(new GuavaSkipsNilVectorFactory());
+        }
+    };
+    
+    abstract Builder newParserConfigBuilder();
+
 }
